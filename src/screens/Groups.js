fa0921472cfb8a7fc85f4b926052fc5ab0ce29ea
Filipe da Silva <silvafilipeuk@gmail.com>
2024-05-23 19:12:09 +0100

    Implemented dynamic rendering of members on groups

--- src/screens/Groups.js
+++ src/screens/Groups.js
@@ -1,41 +1,45 @@
-
-
-import { FlatList, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-import Header from '../components/Header';
-import React, { useState, useEffect } from 'react';
-import Footer from '../components/Footer';
-import { supabase } from '../utils/SupabaseClient';
-import CreateGroup from './CreateGroup';
+import {
+	FlatList,
+	StyleSheet,
+	Text,
+	TouchableOpacity,
+	View,
+} from "react-native";
+import Header from "../components/Header";
+import React, { useState, useEffect } from "react";
+import Footer from "../components/Footer";
+import { supabase } from "../utils/SupabaseClient";
+import CreateGroup from "./CreateGroup";
 
 export default function Groups({ navigation, GlobalState }) {
-
-	const [fetchError, setFetchError] = useState(null)
-	const [groups, setGroups] = useState(null)
+	const [fetchError, setFetchError] = useState(null);
+	const [groups, setGroups] = useState(null);
 
 	useEffect(() => {
-		const fetchGroups = async () =>{
-			const { data, error} = await supabase
-			.from('groups')
-			.select()
+		const fetchGroups = async () => {
+			const { data, error } = await supabase.from("groups").select();
 
-			if(error){
-				setFetchError("cannot fetch groups")
-				setGroups(null)
+			if (error) {
+				setFetchError("cannot fetch groups");
+				setGroups(null);
 			}
-			if(data){
-				setGroups(data)
-				setFetchError(null)
+			if (data) {
+				setGroups(data);
+				setFetchError(null);
 			}
-		}
+		};
 
-		fetchGroups()
-	},[])
-	
+		fetchGroups();
+	}, []);
+
 	const renderBook = ({ item }) => (
 		<TouchableOpacity
 			style={styles.box}
 			onPress={() => {
-				navigation.navigate("Group", { groupName: item.group_name });
+				navigation.navigate("Group", {
+					groupName: item.group_name,
+					groupId: item.group_id,
+				});
 			}}
 		>
 			<Text style={styles.groupName}>{item.group_name}</Text>
@@ -61,37 +65,34 @@ export default function Groups({ navigation, GlobalState }) {
 			<Footer navigation={navigation} GlobalState={GlobalState} />
 		</React.Fragment>
 	);
-
 }
 const styles = StyleSheet.create({
-
-    screen: {
-        flex: 8,
-        alignItems: 'center',
-        justifyContent: 'center',
-        backgroundColor: 'white',
-        padding: '5%',
-        marginTop:60
-    },
-    box: {
-        display: 'flex',
-        alignItems: 'center',
-        backgroundColor: '#E2DFD0',
-        width: '100%',
-        marginTop: 0,
-        marginBottom: 10,
-        borderRadius: 5,
-        padding: 5
-    },
-    button: {
-        marginTop: 10,
-        backgroundColor: '#E6FF94',
-        padding: 10,
-        borderRadius: 5
-    },
-    groupName: {
-        fontSize: 22,
-        fontWeight: 'bold'
-    },
-})
-
+	screen: {
+		flex: 8,
+		alignItems: "center",
+		justifyContent: "center",
+		backgroundColor: "white",
+		padding: "5%",
+		marginTop: 60,
+	},
+	box: {
+		display: "flex",
+		alignItems: "center",
+		backgroundColor: "#E2DFD0",
+		width: "100%",
+		marginTop: 0,
+		marginBottom: 10,
+		borderRadius: 5,
+		padding: 5,
+	},
+	button: {
+		marginTop: 10,
+		backgroundColor: "#E6FF94",
+		padding: 10,
+		borderRadius: 5,
+	},
+	groupName: {
+		fontSize: 22,
+		fontWeight: "bold",
+	},
+});
--- src/screens/Member.js
+++ src/screens/Member.js
@@ -1,67 +1,76 @@
-import { StyleSheet, TouchableOpacity, View, Text, FlatList, Image } from "react-native";
+import {
+	StyleSheet,
+	TouchableOpacity,
+	View,
+	Text,
+	FlatList,
+	Image,
+} from "react-native";
 import Footer from "../components/Footer";
-export default function Member({navigation, route}){
-    const{ name } = route.params;
-    const reviews = [
-        {
-            review: 'blah blah blah blah',
-        },
-        {
-            review: 'blah blah blah blah',
-        },
-        {
-            review: 'blah blah blah blah',
-        },
-    ]
+export default function Member({ navigation, route }) {
+	const { name, id } = route.params;
+	const reviews = [
+		{
+			review: "blah blah blah blah",
+		},
+		{
+			review: "blah blah blah blah",
+		},
+		{
+			review: "blah blah blah blah",
+		},
+	];
 
-    const renderReviews = ({item}) => (
-        <View style={styles.review}>
-            <Image source={require(`../../assets/gatsby.jpg`)} style={styles.Image}/>
-            <Text>{item.review}</Text>
-        </View>
-        
-    )
-    return(
-        <View style={styles.member}>
-             <Text style={styles.name}>{name}</Text>
-             <FlatList 
-             data={reviews}
-             renderItem={renderReviews}
-             keyExtractor={(item, index) => index.toString()}
-             style={styles.list}/>
-             <Footer />
-        </View>
-       
-    )
+	const renderReviews = ({ item }) => (
+		<View style={styles.review}>
+			<Image
+				source={require(`../../assets/gatsby.jpg`)}
+				style={styles.Image}
+			/>
+			<Text>{item.review}</Text>
+		</View>
+	);
+	return (
+		<View style={styles.member}>
+			<Text style={styles.name}>{name}</Text>
+			<FlatList
+				data={reviews}
+				renderItem={renderReviews}
+				keyExtractor={(item, index) => index.toString()}
+				style={styles.list}
+			/>
+			<Footer />
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-    member: {
-        flex:1,
-        backgroundColor: 'white',
-        alignItems: 'center',
-        padding: 10
-    },
-    name: {
-        fontSize: 22,
-        fontWeight: 'bold',
-        padding: 10
-    },
-    list:{
-        width: '100%',
-        height: '75%',
-        backgroundColor: 'white'
-    },
-    review:{
-        width: '100%',
-        flexDirection: 'row',
-        justifyContent: 'space-around',
-        backgroundColor: '#E2DFD0',
-        marginBottom: 10,
-        padding: '5%'
-    },
-    Image: {
-        width: '40%',
-        height: 200,
-    }
-})
+	member: {
+		flex: 1,
+		backgroundColor: "white",
+		alignItems: "center",
+		padding: 10,
+	},
+	name: {
+		fontSize: 22,
+		fontWeight: "bold",
+		padding: 10,
+	},
+	list: {
+		width: "100%",
+		height: "75%",
+		backgroundColor: "white",
+	},
+	review: {
+		width: "100%",
+		flexDirection: "row",
+		justifyContent: "space-around",
+		backgroundColor: "#E2DFD0",
+		marginBottom: 10,
+		padding: "5%",
+	},
+	Image: {
+		width: "40%",
+		height: 200,
+	},
+});
