d918feff1232f843f6797b58910959ff892edc65
Filipe da Silva <silvafilipeuk@gmail.com>
2024-05-28 15:42:54 +0100

    Users can now add a review to a book

--- src/components/BookCard.js
+++ src/components/BookCard.js
@@ -1,46 +1,39 @@
-import { StyleSheet, Text, View, Image, FlatList } from 'react-native';
+import { StyleSheet, Text, View, Image, FlatList } from "react-native";
 
 export default function BookCard({ book }) {
-  const thumbnail = book.thumbnail;
-  const author = book.author;
-  const title = book.title;
+	const thumbnail = book.thumbnail;
+	const author = book.author;
+	const title = book.title;
 
-  return (
-    <View style = {styles.container}>
-      <Image
-        style = {styles.cover}
-        source = {{ uri: thumbnail }}
-      />
-      <Text style = {styles.text}> {title} </Text>
-      <Text style = {styles.text}> {author} </Text>
-    </View>
-  );
+	return (
+		<View style={styles.container}>
+			<Image style={styles.cover} source={{ uri: thumbnail }} />
+			<Text style={styles.textTitle}> {title} </Text>
+			<Text style={styles.textAuthor}> {author} </Text>
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    //flex: 1,
-    width: "100%",
-    alignItems: 'center',
-    justifyContent: 'center',
-    shadowColor: "#000",
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
-    shadowOpacity: 0.25,
-    shadowRadius: 3.84,
-    elevation: 5,
-    zIndex: 5
-  },
-  text: {
-    fontSize: 18,
-    fontWeight: "400",
-    paddingTop: 6
-  },
-  cover: {
-    width: 300,
-    height: 300,
-    resizeMode: 'contain',
-  }
+	container: {
+		//flex: 1,
+		width: "100%",
+		alignItems: "center",
+		justifyContent: "center",
+	},
+	textTitle: {
+		fontSize: 18,
+		fontWeight: "400",
+		paddingTop: 20,
+		paddingBottom: 10,
+	},
+	textAuthor: {
+		fontSize: 18,
+		fontWeight: "400",
+	},
+	cover: {
+		width: 300,
+		height: 300,
+		resizeMode: "contain",
+	},
 });
